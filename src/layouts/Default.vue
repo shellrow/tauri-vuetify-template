106abf2c6867f7de9d0ae249ffb78431dffd871e
shellrow <81893184+shellrow@users.noreply.github.com>
2023-07-01 11:23:03 +0900

    Update Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -50,7 +50,6 @@
     </v-navigation-drawer>
     <v-main>
         <v-app-bar app>
-        <v-app-bar-nav-icon @click.stop="rail = !rail"></v-app-bar-nav-icon>
         <v-toolbar-title>Application</v-toolbar-title>
         </v-app-bar>
         <v-container fluid>
